28c36b2d467b5217686872b0f798528963c50b20
SFRMilkTea <bayliz@bk.ru>
2023-03-20 20:36:11 +0700

    Личный кабинет

--- guitarStoreApp/urls.py
+++ guitarStoreApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView, GuitarListView, SearchView, GuitarView
+from .views import HomePageView, GuitarListView, SearchView, GuitarView, LkView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('product/detail/<int:id_product>/', GuitarView.product_detail, name='product_detail'),
     path('products', GuitarListView.as_view(), name='products'),
+    path('lk', LkView.as_view(), name='lk'),
     path('search', SearchView.as_view(), name='search'),
 ]
--- guitarStoreApp/views.py
+++ guitarStoreApp/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
-from django.views import View
-from django.views.generic import TemplateView, ListView, FormView
+
+from django.views.generic import TemplateView, ListView
 
 from cart.forms import CartAddProductForm
 from guitarStoreApp.models import Product, Producer
+from orders.models import Order
 
 
 class HomePageView(TemplateView):
@@ -18,7 +19,6 @@ class GuitarListView(ListView):
 
 
 class GuitarView(TemplateView):
-
     template_name = 'product/detail.html'
     model = Product
     context_object_name = "product"
@@ -47,10 +47,12 @@ class SearchView(ListView):
             Q(id_producer__producer__icontains=query)).reverse()
 
 
-def product_detail(request, id_product):
-    product = get_object_or_404(Product,
-                                id_product=id_product,
-                                available=True)
-    cart_product_form = CartAddProductForm()
-    return render(request, 'product/detail.html', {'product': product,
-                                                   'cart_product_form': cart_product_form})
+class LkView(ListView):
+    template_name = 'lk.html'
+    model = Order
+    context_object_name = "list_of_all_orders"
+
+    def get_queryset(self):
+        query = self.request.user.username
+        return Order.objects.filter(
+            Q(user__username__icontains=query)).reverse()
