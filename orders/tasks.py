4188458485e7f878295285cc9e4e3749da358ed8
SFRMilkTea <bayliz@bk.ru>
2023-04-12 17:56:00 +0700

    отправка сообщений

---
+++ orders/tasks.py
@@ -0,0 +1,20 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from .models import Order
+
+
+@shared_task
+def order_created(order_id):
+    """
+    Задача для отправки уведомления по электронной почте при успешном создании заказа.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = 'Заказ №'.format(order_id)
+    message = 'Уважаемый {},\n\nспасибо за оформление заказа.\
+                Номер вашего заказа: {}.'.format(order.first_name,
+                                                 order.id)
+    mail_sent = send_mail(subject,
+                          message,
+                          'bayliz@bk.ru',
+                          [order.email])
+    return mail_sent
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView, ListView
-
+from .tasks import *
 from cart.cart import Cart
 from orders.models import Order
 from .forms import OrderCreateForm
@@ -21,7 +21,7 @@ class OrderView(TemplateView):
                 address = form.cleaned_data['address']
                 order = Order.objects.create(user=user,
                                              address=address,
-                                             first_name = user.first_name,
+                                             first_name=user.first_name,
                                              last_name=user.last_name,
                                              email=user.email,
                                              )
@@ -32,6 +32,8 @@ class OrderView(TemplateView):
                                              quantity=item['quantity'])
                 # очистка корзины
                 cart.clear()
+                # запуск асинхронной задачи
+                order_created.delay(order.id)
                 return render(request, 'orders/order/created.html',
                               {'order': order})
         else:
@@ -49,4 +51,3 @@ class OrderItemsListView(ListView):
     template_name = 'order/detail.html'
     model = OrderItem
     context_object_name = "list_of_all_order_items"
-
